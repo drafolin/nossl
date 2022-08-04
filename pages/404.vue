212ea4e403b4058a52da57179f12b86784353f92
Odin Beuchat <66629792+dindin-nibnib@users.noreply.github.com>
2022-08-05 01:10:42 +0200

    Added 404 page

---
+++ pages/404.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+onMounted(() => {
+	window.location.href = "http://wifi.dindin-nibnib.com"
+})
+</script>
+
+<template>
+	<div>
+		Wait for redirection...
+	</div>
+</template>
