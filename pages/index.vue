ff59d8a50dc958aea6801ba6b0b5709a54881893
Odin Beuchat <66629792+dindin-nibnib@users.noreply.github.com>
2022-08-05 18:40:08 +0200

    added icon

--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,13 @@ useHead({
 	<div class="w-full h-full bg-white dark:bg-slate-900">
 		<div class="flex items-center w-screen h-screen">
 			<div class="flex flex-col items-center w-screen">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-10 h-10 animate-pulse">
+					<!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+					<g color="lime">
+						<path fill="currentColor"
+							d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM371.8 211.8C382.7 200.9 382.7 183.1 371.8 172.2C360.9 161.3 343.1 161.3 332.2 172.2L224 280.4L179.8 236.2C168.9 225.3 151.1 225.3 140.2 236.2C129.3 247.1 129.3 264.9 140.2 275.8L204.2 339.8C215.1 350.7 232.9 350.7 243.8 339.8L371.8 211.8z" />
+					</g>
+				</svg>
 				<h1 class="m-6 text-6xl font-bold">Connected!</h1>
 				<div>You are connected to the internet!</div>
 			</div>
